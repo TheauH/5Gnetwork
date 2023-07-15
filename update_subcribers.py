9b3d41e11d80ca4a0709d4f4d00d0ad2f879c1f8
vagrant <vagrant@comnetsemu>
2023-07-15 09:16:28 +0000

    Last modification to the configurations and to the example 3.

--- update_subcribers.py
+++ update_subcribers.py
@@ -15,7 +15,7 @@ import json, time
 
 if __name__ == "__main__":
 
-    prj_folder="/home/vagrant/comnetsemu/app/comnetsemu_5Gnet"
+    prj_folder="/home/vagrant/comnetsemu/app/5Gnetwork"
     
     print(f"*** Open5GS: Init subscriber for UE 0")
     o5gs   = Open5GS( "172.17.0.2" ,"27017")
@@ -23,4 +23,7 @@ if __name__ == "__main__":
     with open( prj_folder + "/python_modules/subscriber_profile_1.json" , 'r') as f:
         profile = json.load( f )
     o5gs.addSubscriber(profile)
+    # with open( prj_folder + "/python_modules/subscriber-profile2_gnb2.json" , 'r') as file:
+    #     profile1 = json.load( file )
+    # o5gs.addSubscriber(profile1)
 
